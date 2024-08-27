8525bf54885f4420b6456deccd4e605a9c30ecea
Haroon Albar <haroonalbarx@gmail.com>
2024-08-27 15:35:30 +0530

    serve from main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/haroonalbar/book-store/pkg/routes"
+
+	// mysql driver
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
 func main() {
+	r := mux.NewRouter()
+
+	routes.ResgisterBookStoreRoutes(r)
+	http.Handle("/", r)
+
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }

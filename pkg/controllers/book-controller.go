58649bfe03549e2cf138c366ae18da8acc1ee9db
Haroon Albar <haroonalbarx@gmail.com>
2024-08-31 21:51:14 +0530

    feat: added all controllers

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,18 +1,99 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
 
-func CreateBook(w http.ResponseWriter, r *http.Request) {
-}
+	"github.com/gorilla/mux"
+	"github.com/haroonalbar/book-store/pkg/models"
+	"github.com/haroonalbar/book-store/pkg/utils"
+)
+
+var Book models.Book
 
 func GetBook(w http.ResponseWriter, r *http.Request) {
+	books := models.GetAllBooks()
+	res, _ := json.Marshal(books)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func GetBookByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["id"]
+	id, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		http.Error(w, "Error while parsing id", http.StatusBadRequest)
+		return
+	}
+	book, _ := models.GetBookByID(id)
+	if err != nil {
+		http.Error(w, "Error getting book", http.StatusBadRequest)
+		return
+	}
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
-func UpdateBook(w http.ResponseWriter, r *http.Request) {
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	book := &models.Book{}
+	// from utils
+	utils.ParseBody(r, book)
+	// from models
+	b := book.CreateBook()
+	res, _ := json.Marshal(b)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookID := vars["id"]
+	id, err := strconv.ParseInt(bookID, 0, 0)
+	if err != nil {
+		http.Error(w, "Error while parsing id", http.StatusBadRequest)
+		return
+	}
+	book := models.DeleteBook(id)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	updateBook := &models.Book{}
+	// read from body and put it on book
+	utils.ParseBody(r, updateBook)
+	vars := mux.Vars(r)
+	bookID := vars["id"]
+	id, err := strconv.ParseInt(bookID, 0, 0)
+	if err != nil {
+		http.Error(w, "Error while parsing id", http.StatusBadRequest)
+		return
+	}
+	book, db := models.GetBookByID(id)
+
+	if updateBook.Name != "" {
+		book.Name = updateBook.Name
+	}
+	if updateBook.Author != "" {
+		book.Author = updateBook.Author
+	}
+	if updateBook.Publication != "" {
+		book.Publication = updateBook.Publication
+	}
+	// book.UpdatedAt = time.Now().UTC()
+
+	db.Save(&book)
+
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -10,7 +10,7 @@ import (
 var ResgisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods(http.MethodPost)
 	router.HandleFunc("/book/", controllers.GetBook).Methods(http.MethodGet)
-	router.HandleFunc("/book/{bookID}", controllers.GetBookByID).Methods(http.MethodGet)
-	router.HandleFunc("/book/{bookID}", controllers.UpdateBook).Methods(http.MethodPut)
-	router.HandleFunc("/book/{bookID}", controllers.DeleteBook).Methods(http.MethodDelete)
+	router.HandleFunc("/book/{id}", controllers.GetBookByID).Methods(http.MethodGet)
+	router.HandleFunc("/book/{id}", controllers.UpdateBook).Methods(http.MethodPut)
+	router.HandleFunc("/book/{id}", controllers.DeleteBook).Methods(http.MethodDelete)
 }
